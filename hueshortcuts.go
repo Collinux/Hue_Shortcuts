caf99402e7ad48660eb9ed87a1d73188eb6464db
Collin Guarino <collin.guarino@gmail.com>
2016-02-24 20:43:35 -0500

    Added license header and redid code structure to work with new library update.

--- hueshortcuts.go
+++ hueshortcuts.go
@@ -1,3 +1,15 @@
+/*
+* hueshortcuts.go
+* Uses a Philips Hue go library to enable command
+* line shortcuts for light functions.
+* Copyright (C) 2016 Collin Guarino (Collinux) collin.guarino@gmail.com
+* License: GPL version 2 or higher http://www.gnu.org/licenses/gpl.html
+*/
+// Example: go run hueshortcuts.go all_on
+// Example: go run hueshortcuts.go all_off
+// Example: go run hueshortcuts.go writing_code_scene
+
+
 package main
 
 import (
@@ -7,47 +19,37 @@ import (
 )
 
 func main() {
-    bridge, _ := hue.NewBridge("192.168.1.128")
-    bridge.Username = "427de8bd6d49f149c8398e4fc08f"
+    bridge, _ := hue.NewBridge("192.168.1.128") // Enter your own bridge IP
+    bridge.Username = "427de8bd6d49f149c8398e4fc08f" // Enter your own user
 
+    lights, _ := bridge.GetAllLights()
     switch os.Args[1] { // Index 0 arg would be the file name
         case "all_on":
             fmt.Println("calling all_on")
-            for index := 0; index <= 10; index++ {
-                light, err := bridge.GetLightByIndex(index)
-                if err != nil {
-                    continue
-                }
+            for _, light := range lights {
                 light.On()
                 light.SetColor(hue.WHITE)
             }
             break
         case "all_off":
             fmt.Println("calling all off")
-            for index := 0; index <= 10; index++ {
-                light, err := bridge.GetLightByIndex(index)
-                if err != nil {
-                    continue
-                }
+            for _, light := range lights {
                 light.Off()
             }
             break
         case "writing_code_scene":
-            for index := 0; index <= 10; index++ {
-                light, err := bridge.GetLightByIndex(index)
-                if err != nil {
-                    continue
-                }
+            for _, light := range lights {
                 if light.Name == "Desk Light" {
                     light.On()
                     light.SetColor(hue.WHITE)
                 } else if light.Name == "Bedroom Lamp" {
                     light.On()
                     light.SetColor(hue.BLUE)
-                    light.Dim(20)
+                    light.SetBrightness(70)
                 } else {
                     light.Off()
                 }
             }
+            break
     }
 }
